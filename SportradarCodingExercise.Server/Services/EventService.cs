f06bdfead735627409a591a9599f0b438b04152c
kroli98 <113181774+kroli98@users.noreply.github.com>
2024-11-13 23:04:35 +0100

    Add EventService class with not yet implemented functions based on IEventService

---
+++ SportradarCodingExercise.Server/Services/EventService.cs
@@ -0,0 +1,33 @@
+﻿using SportradarCodingExercise.Server.Interfaces;
+using SportradarCodingExercise.Server.Models;
+
+namespace SportradarCodingExercise.Server.Services
+{
+    public class EventService : IEventService
+    {
+        public Task<Event> AddEventAsync(Event evnt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Event> GetEventByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Event>> GetEventsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Event>> GetEventsByDateAsync(DateOnly date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Event>> GetEventsBySportIdAsync(int sportId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
