3f3965be0f0a2e21d8fb908c8779edb9e00d5acd
kroli98 <113181774+kroli98@users.noreply.github.com>
2024-11-13 22:59:45 +0100

    Add IEventService interface for event operations

---
+++ SportradarCodingExercise.Server/Interfaces/IEventService.cs
@@ -0,0 +1,13 @@
+﻿using SportradarCodingExercise.Server.Models;
+
+namespace SportradarCodingExercise.Server.Interfaces
+{
+    public interface IEventService
+    {
+        Task<IEnumerable<Event>> GetEventsAsync();
+        Task<IEnumerable<Event>> GetEventsByDateAsync(DateOnly date);
+        Task<IEnumerable<Event>> GetEventsBySportIdAsync(int sportId);
+        Task<Event> GetEventByIdAsync(int id);
+        Task<Event> AddEventAsync(Event evnt);
+    }
+}
