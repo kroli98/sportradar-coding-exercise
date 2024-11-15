1d393567bee5b53f8225fea15fbe7ed88e93aa7a
kroli98 <113181774+kroli98@users.noreply.github.com>
2024-11-15 20:04:36 +0100

    Add EventController to test response for getting events

---
+++ SportradarCodingExercise.Server/Controllers/EventController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SportradarCodingExercise.Server.Interfaces;
+using SportradarCodingExercise.Server.Models;
+
+namespace SportradarCodingExercise.Server.Controllers
+{
+    [Route("api/sport-events")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly IEventService _eventService;
+
+        public EventController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Event>>> GetEvents()
+        {
+            var events = await _eventService.GetEventsAsync();
+
+            if(!events.Any())
+            {
+                return NoContent();
+            }
+
+            return Ok(events);
+        }
+    }
+}
