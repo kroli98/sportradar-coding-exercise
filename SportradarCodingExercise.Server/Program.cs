e3e8c97d88d62c6600f162fa4856eb1983b7470f
kroli98 <113181774+kroli98@users.noreply.github.com>
2024-11-15 13:49:42 +0100

    Add EventService to services

--- SportradarCodingExercise.Server/Program.cs
+++ SportradarCodingExercise.Server/Program.cs
@@ -1,4 +1,6 @@
 using SportradarCodingExercise.Server.Data;
+using SportradarCodingExercise.Server.Interfaces;
+using SportradarCodingExercise.Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<DatabaseInitializer>();
+builder.Services.AddScoped<IEventService, EventService>();
 
 var app = builder.Build();
 
