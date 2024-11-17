2d6522d4f8ef95abebcc7a0751c203d9a04ed9e8
kroli98 <113181774+kroli98@users.noreply.github.com>
2024-11-17 14:24:05 +0100

    Add CreateEventDto for POST requests

---
+++ SportradarCodingExercise.Server/DTOs/CreateEventDto.cs
@@ -0,0 +1,22 @@
+﻿using SportradarCodingExercise.Server.Models;
+
+namespace SportradarCodingExercise.Server.DTOs
+{
+    public class CreateEventDto
+    {
+        public required DateOnly Date { get; set; }
+        public required TimeOnly TimeUTC { get; set; }
+        public required int DurationInMinutes { get; set; }
+        public string? Description { get; set; }
+        public required int HomeScore { get; set; }
+        public required int AwayScore { get; set; }
+
+        public required int StatusId { get; set; }
+        public required int HomeTeamId { get; set; }
+        public required int AwayTeamId { get; set; }
+        public required int VenueId { get; set; }
+        public required int StageId { get; set; }
+        public required int CompetitionId { get; set; }
+        public required int SportId { get; set; }
+    }
+}
