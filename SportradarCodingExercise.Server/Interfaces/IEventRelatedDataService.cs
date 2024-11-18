9cd3ad4448a25c35cb560798486b5059cc68bb2a
kroli98 <113181774+kroli98@users.noreply.github.com>
2024-11-18 19:01:10 +0100

    Add IEventRelatedDataService interface for event related operations

---
+++ SportradarCodingExercise.Server/Interfaces/IEventRelatedDataService.cs
@@ -0,0 +1,16 @@
+﻿using SportradarCodingExercise.Server.Models;
+
+namespace SportradarCodingExercise.Server.Interfaces
+{
+    public interface IEventRelatedDataService
+    {
+        Task<IEnumerable<Status>> GetStatusesAsync();
+        Task<IEnumerable<Team>> GetTeamsAsync();
+        Task<IEnumerable<Venue>> GetVenuesAsync();
+        Task<IEnumerable<Stage>> GetStagesAsync();
+        Task<IEnumerable<Competition>> GetCompetitionsAsync();
+        Task<IEnumerable<Sport>> GetSportsAsync();
+        Task<IEnumerable<Season>> GetSeasonsAsync();
+
+    }
+}
