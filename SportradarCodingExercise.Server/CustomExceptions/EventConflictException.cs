c16c8ae05b111e621ebf1e8d9353958f721543f3
kroli98 <113181774+kroli98@users.noreply.github.com>
2024-11-18 13:30:49 +0100

    Add EventConflictException class and EventConflictType enum to handle response for event conflictions

---
+++ SportradarCodingExercise.Server/CustomExceptions/EventConflictException.cs
@@ -0,0 +1,19 @@
+﻿namespace SportradarCodingExercise.Server.CustomExceptions
+{
+    public class EventConflictException : Exception
+    {
+        public EventConflictType ConflictType { get; }
+
+        public EventConflictException(string message, EventConflictType conflictType)
+            : base(message)
+        {
+            ConflictType = conflictType;
+        }
+    }
+
+    public enum EventConflictType
+    {
+        Venue,
+        Team
+    }
+}
