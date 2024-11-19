b91a2e34050e9a01769a31d8594433a0193aa16f
kroli98 <113181774+kroli98@users.noreply.github.com>
2024-11-19 11:30:36 +0100

    Add EventDetailDto for GET requests

---
+++ SportradarCodingExercise.Server/DTOs/EventDetailDto.cs
@@ -0,0 +1,13 @@
+﻿using SportradarCodingExercise.Server.Models;
+
+namespace SportradarCodingExercise.Server.DTOs
+{
+    public class EventDetailDto
+    {
+        public required int EventDetailId { get; set; }
+        public required DateTime RecordedAtUTC { get; set; }
+        public string? Description { get; set; }
+        public required Team Team { get; set; }
+        public required EventType EventType { get; set; }
+    }
+}
