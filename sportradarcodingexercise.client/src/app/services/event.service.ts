506e035744ca9ad94a2a7787b2dcea870d5bcfc2
kroli98 <113181774+kroli98@users.noreply.github.com>
2024-11-20 22:13:48 +0100

    Add EventService to handle Event related api interactions

---
+++ sportradarcodingexercise.client/src/app/services/event.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getEvents() {
+    return this.http.get<Event[]>(this.baseUrl + '/sport-events');
+  }
+
+  getEvent(eventId: number) {
+    return this.http.get<Event>(this.baseUrl + '/sport-events/id/' + eventId);
+  }
+
+  getEventByDate(date: string) {
+    return this.http.get<Event>(this.baseUrl + '/sport-events/date/' + date);
+  }
+
+  getEventBySportId(sportId: number) {
+    return this.http.get<Event>(this.baseUrl + '/sport-events/sport/' + sportId);
+  }
+}
