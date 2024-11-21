1590ea7fc937949f613b366c05b51395996ba373
kroli98 <113181774+kroli98@users.noreply.github.com>
2024-11-21 21:13:29 +0100

    Add components to declarations and routing to imports

--- sportradarcodingexercise.client/src/app/app.module.ts
+++ sportradarcodingexercise.client/src/app/app.module.ts
@@ -3,13 +3,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventListComponent } from './lists/event-list/event-list.component';
+import { NavComponent } from './nav/nav.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventListComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
