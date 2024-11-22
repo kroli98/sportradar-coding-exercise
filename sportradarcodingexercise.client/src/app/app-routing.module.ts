47029a31599bcaa1cc8cf51327e4a0c1b71968f9
kroli98 <113181774+kroli98@users.noreply.github.com>
2024-11-22 16:56:32 +0100

    Add route for AddEventComponent in app-routing.module.ts

--- sportradarcodingexercise.client/src/app/app-routing.module.ts
+++ sportradarcodingexercise.client/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { EventListComponent } from './lists/event-list/event-list.component';
+import { AddEventComponent } from './add-event/add-event.component';
 
 const routes: Routes = [
 
   { path: '', redirectTo: '/events', pathMatch: 'full' },
-  { path: 'events', component: EventListComponent}
+  { path: 'events', component: EventListComponent },
+  { path: 'add-event', component: AddEventComponent}
 
 
   ];
