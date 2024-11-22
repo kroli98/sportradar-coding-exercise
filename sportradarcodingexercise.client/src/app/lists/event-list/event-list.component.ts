8764bf2bee0107fff6e99ff705e9b8598c3acb44
kroli98 <113181774+kroli98@users.noreply.github.com>
2024-11-22 13:53:49 +0100

    Implement EventListComponent with list of cards showing event infos. Handle error and loading states

--- sportradarcodingexercise.client/src/app/lists/event-list/event-list.component.ts
+++ sportradarcodingexercise.client/src/app/lists/event-list/event-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { EventService } from '../../services/event.service';
+import { Event } from '../../models/event';
+
 
 @Component({
   selector: 'app-event-list',
@@ -7,4 +10,39 @@ import { Component } from '@angular/core';
 })
 export class EventListComponent {
 
+  events: Event[] = []
+  loading = true;
+  error: string | null = null;
+
+  constructor(private readonly eventService: EventService) { }
+
+  ngOnInit() {
+    this.loadEvents();
+  }
+
+  loadEvents() {
+    this.loading = true;
+    this.error = null;
+
+    this.eventService.getEvents().subscribe({
+      next: (events) => {
+        this.events = events;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Error loading events:', error);
+        this.error = 'Failed to load sports events. Please try again later.';
+        this.loading = false;
+      }
+    });
+  }
+
+  retry() {
+    this.loadEvents();
+  }
+
+  formatTime(timeUTC: string): string {
+    const timeParts = timeUTC.split(':');
+    return `${timeParts[0]}:${timeParts[1]}`;
+  }
 }
