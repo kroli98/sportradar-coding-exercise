e6e888471df34e9781b7d89e0183f11634623b13
kroli98 <113181774+kroli98@users.noreply.github.com>
2024-11-23 10:19:17 +0100

    Add EventRelatedDataService to handle Event related data api interactions

---
+++ sportradarcodingexercise.client/src/app/services/event-related-data.service.ts
@@ -0,0 +1,48 @@
+// services/event-related-data.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+import { Competition } from '../models/competition';
+import { Season } from '../models/season';
+import { Sport } from '../models/sport';
+import { Stage } from '../models/stage';
+import { Status } from '../models/status';
+import { Team } from '../models/team';
+import { Venue } from '../models/venue';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventRelatedDataService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private readonly http: HttpClient) { }
+
+  getSports() {
+    return this.http.get<Sport[]>(this.baseUrl + '/sports');
+  }
+
+  getCompetitions() {
+    return this.http.get<Competition[]>(this.baseUrl + '/competitions');
+  }
+
+  getSeasons() {
+    return this.http.get<Season[]>(this.baseUrl + '/seasons');
+  }
+
+  getStages() {
+    return this.http.get<Stage[]>(this.baseUrl + '/stages');
+  }
+
+  getStatuses() {
+    return this.http.get<Status[]>(this.baseUrl + '/statuses');
+  }
+
+  getTeams() {
+    return this.http.get<Team[]>(this.baseUrl + '/teams');
+  }
+
+  getVenues() {
+    return this.http.get<Venue[]>(this.baseUrl + '/venues');
+  }
+}
